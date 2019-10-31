909ef344b27655e358868eee70c5134e248c329a
Justin Susanto <39823674+hawkjstn98@users.noreply.github.com>
2019-10-31 23:57:31 +0700

    remove Id From Object to Prevent Strange Thing on Mongo

--- main/entity/object/user/user.go
+++ main/entity/object/user/user.go
@@ -5,7 +5,6 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 const UserCollection = "user"
 
 type User struct {
-	Id         	 	primitive.ObjectID `json:"id" bson:"_id"`
 	PhoneNumber 	string  `json:"phoneNumber" bson:"phoneNumber"`
 	Timestamp  	 	primitive.DateTime `json:"timestamp" bson:"timestamp"`
 	Email 			string `json:"email" bson:"email"`
