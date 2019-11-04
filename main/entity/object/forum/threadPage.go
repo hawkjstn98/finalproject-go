c7c2b20f72873a656b2315fd954c46f28bc1d819
Vionie Laorensa <39825885+laoPhio49@users.noreply.github.com>
2019-11-04 13:21:13 +0700

    forum home

--- main/entity/object/forum/threadPage.go
+++ main/entity/object/forum/threadPage.go
@@ -2,15 +2,13 @@ package forum
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-const ThreadCollection = "thread"
-
-type Thread struct {
+type ThreadPage struct {
 	Id				primitive.ObjectID `json:"id" bson:"_id"`
 	Timestamp  	 	primitive.DateTime `json:"timestamp" bson:"timestamp"`
 	Name			string `json:"name" bson:"name"`
 	Category		string `json:"category" bson:"category"`
 	MakerUsername	string `json:"makerUsername" bson:"makerUsername"`
+	MakerImage		string `json:"makerImage" bson:"makerImage"`
 	Description		string `json:"description" bson:"description"`
-	CommentList		[]ObjectComment `json:"commentList" bson:"commentList"`
+	CommentNumber	int `json:"commentNumber" bson:"commentNumber"`
 }
-
--- main/repository/thread_repository/threadRepository.go
+++ main/repository/thread_repository/threadRepository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/hawkjstn98/FinalProjectEnv/main/entity/constant/mongo_constant"
 	"github.com/hawkjstn98/FinalProjectEnv/main/entity/object/forum"
+	"github.com/hawkjstn98/FinalProjectEnv/main/entity/object/user"
 	"github.com/hawkjstn98/FinalProjectEnv/main/helper/dbhealthcheck"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
@@ -11,8 +12,9 @@ import (
 
 var client = dbhealthcheck.Conf.MongoClient
 var threadCollection = client.Database(mongo_constant.DBName).Collection(forum.ThreadCollection)
+var userCollection = client.Database(mongo_constant.DBName).Collection(user.UserCollection)
 
-func GetThreadPage() (result []*forum.Thread) {
+func GetThreadPage() (result []*forum.ThreadPage) {
 	cursor, err := threadCollection.Find(context.TODO(), bson.D{{}})
 
 	if err != nil{
@@ -22,18 +24,39 @@ func GetThreadPage() (result []*forum.Thread) {
 
 	for cursor.Next(context.Background()){
 		var thread forum.Thread
+		//log.Println("Cursor: ", cursor)
 		err := cursor.Decode(&thread)
-		result = append(result, &thread)
+		//log.Println("Decode: ", &thread)
+
+		//
 		if err != nil {
 			log.Println("Data Error", err)
+			return
 		}
+		//log.Println("Thread: ", thread.MakerUsername)
+		filter := bson.M{"username": thread.MakerUsername}
+		var userThread user.User
+		ctx := context.Background()
+		cursorUser := userCollection.FindOne(ctx, filter)
+		cursorUser.Decode(&userThread)
+		log.Println("cursorUser: ", &userThread)
+
+		var currThread forum.ThreadPage
+
+		currThread.Id = thread.Id
+		currThread.Timestamp = thread.Timestamp
+		currThread.Name = thread.Name
+		currThread.Category = thread.Category
+		currThread.MakerUsername = thread.MakerUsername
+		currThread.MakerImage = userThread.ProfileImage
+		currThread.Description = thread.Description
+		currThread.CommentNumber = len(thread.CommentList)
+
+		result = append(result, &currThread)
 	}
+
+
 
 	return result
 
-	// err := threadCollection.Find(nil).All(&result)
-	// if err != nil {
-	// 	log.Println("Data Error: ", err)
-	// }
-	// return result
 }
