0b40fb9279ba4954dffec36690bdf862d494db41
Justin Susanto <39823674+hawkjstn98@users.noreply.github.com>
2019-11-10 14:50:30 +0700

    Fix Cors By Adding Cors Configuration

--- main/main.go
+++ main/main.go
@@ -4,11 +4,17 @@ import (
 	"github.com/hawkjstn98/FinalProjectEnv/main/route"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -17,9 +23,6 @@ func main() {
 	// Routes
 	route.MainRouter(e)
 
-
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
-
-
