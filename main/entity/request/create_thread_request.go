267c285d2381957aba6b4d45d3c16bfbc98cd73b
frapslover63 <39822385+frapslover63@users.noreply.github.com>
2019-11-16 23:19:13 +0700

    swagger

--- main/entity/request/create_thread_request.go
+++ main/entity/request/create_thread_request.go
@@ -15,8 +15,8 @@ type CreateThreadRequest struct {
 type CreateThreadCommentRequest struct {
 	MasterThreadID string `json:"threadMasterId" bson:"threadMasterId"`
 	Timestamp  	 	time.Time `json:"timestamp" bson:"timestamp"`
-	Name			string `json:"name" bson:"name"`
 	Category		string `json:"category" bson:"category"`
 	MakerUsername	string `json:"makerUsername" bson:"makerUsername"`
+	MakerImage    string    `json:"makerImage" bson:"makerImage"`
 	Description		string `json:"description" bson:"description"`
 }
